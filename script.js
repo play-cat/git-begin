607cd8a76041bc122aa5cb1c088d4a527163b95f
Oleksandr Sivets <localhost83@gmail.com>
2025-05-21 23:58:16 +0300

    add second API

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.log('Hello Git');
 
 const PI = 3.14;
 const API_1 = 'https://jsonplaceholder.typicode.com/posts';
+const API_2 = 'https://jsonplaceholder.typicode.com/users';
 class Person {
   constructor(name, age) {
     this.name = name;
